cd6b5333d5a4ac0f32048e15b4bb37d9dde4c6ac
Gabriel <gabriel02ramos@gmail.com>
2025-09-27 19:36:02 -0300

    new render-pt2

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:8080',
-    'https://dinos-s.github.io/chocolatre-PE/'
+    'https://dinos-s.github.io/'
 ];
 
 const app = express();
